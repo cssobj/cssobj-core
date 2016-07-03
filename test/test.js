b176a670c8580b92345b656347304c5d44f064c6
1111hui <26634873@qq.com>
2016-07-03 16:08:51 +0800

    FIX: newline in prop be removed

--- test/test.js
+++ test/test.js
@@ -40,6 +40,27 @@ describe('test cssobj', function(){
     cssobj = require('../lib/cssobj.js')
   })
 
+  //
+  // option test
+  describe('test cssobj options', function() {
+
+    it('css with 2 space indent', function() {
+      var ret = cssobj(
+        {p:{color:'red'}},
+        {indent:'  '}
+      )
+      expect(ret.css.trim()).deep.equal(
+`p
+{
+  color: red;
+}`
+      )
+    })
+
+  })
+
+  //
+  // function test
   describe('test selector without class', function() {
 
     it('css from underline properties', function() {
@@ -87,13 +108,175 @@ describe('test cssobj', function(){
       )
     })
 
-    it('css with common.css', function() {
+    it('single child selector', function() {
+      var ret = cssobj({div:{
+        'fontSize':'12px',
+        'p':{
+          color:'red'
+        }
+      }}, {indent:'  '})
+      expect(ret.css.trim()).deep.equal(
+`div p
+{
+  color: red;
+}
+div
+{
+  font-size: 12px;
+}`
+      )
+    })
+
+    it('comma in child selector', function() {
+      var ret = cssobj({'div,table':{
+        'fontSize':'12px',
+        'p,span':{
+          color:'red'
+        }
+      }}, {indent:'  '})
+      expect(ret.css.trim()).deep.equal(
+`div p, table p,div span, table span
+{
+  color: red;
+}
+div, table
+{
+  font-size: 12px;
+}`
+      )
+    })
+
+    it('using & in child selector', function() {
+      var ret = cssobj({div:{
+        'fontSize':'12px',
+        '&:before, &:after':{
+          'content':'"---"'
+        }
+      }}, {indent:'  '})
+      expect(ret.css.trim()).deep.equal(
+`div:before, div:after
+{
+  content: "---";
+}
+div
+{
+  font-size: 12px;
+}`
+      )
+    })
+
+    it('selector with attribute []', function() {
+      var ret = cssobj({'p[title="abc"]':{color:'red'}})
+      expect(ret.css.trim()).deep.equal(
+`p[title="abc"]
+{
+	color: red;
+}`
+      )
+    })
+
+    xit('selector with comma in attribute []', function() {
+      var ret = cssobj({'p[title="abc"]':{color:'red'}})
+      expect(ret.css.trim()).deep.equal(
+`p[title="abc"]
+{
+	color: red;
+}`
+      )
+    })
+
+    xit('css with reset.css', function() {
       var ret = cssobj({p:{'_fontSize':'12px', 'background\\Color':'#fff'}}, {propSugar:0})
       expect(ret.css.trim()).deep.equal(
 `p
 {
 	_fontSize: 12px;
 	background\\Color: #fff;
+}`
+      )
+    })
+
+
+  })
+
+  //
+  // test with local class name
+  describe('test with local class name', function() {
+
+    it('local class name with random string', function() {
+      // random string cannot test right,
+      // so using regexp to test format
+      var ret = cssobj({'.red':{
+        'color':'red',
+      }})
+          .css
+          .trim()
+          .split(/\n/)
+      expect(ret.shift()).match(/^._\w{6,7}\d+_red/)
+      expect(ret.join('\n')).equal(
+`{
+	color: red;
+}`
+      )
+    })
+
+    // below will using _prefix_ as prefix
+    it('local class name with custom prefix', function() {
+      var ret = cssobj({'.red':{
+        'color':'red',
+      }}, {prefix: '_prefix_'}).css.trim()
+      expect(ret).equal(
+`._prefix_red
+{
+	color: red;
+}`
+      )
+    })
+
+    it('local class name with :global escape 1', function() {
+      var ret = cssobj({':global(.red).bold':{
+        'color':'red',
+      }}, {prefix: '_prefix_'}).css.trim()
+      expect(ret).equal(
+`.red._prefix_bold
+{
+	color: red;
+}`
+      )
+    })
+
+    it('local class name with :global escape 2', function() {
+      var ret = cssobj({':global(.red.green .blue).bold':{
+        'color':'red',
+      }}, {prefix: '_prefix_'}).css.trim()
+      expect(ret).equal(
+`.red.green .blue._prefix_bold
+{
+	color: red;
+}`
+      )
+    })
+
+    it('local class name with ! escape 1', function() {
+      var ret = cssobj({'.!red.bold':{
+        'color':'red',
+      }}, {prefix: '_prefix_'}).css.trim()
+      expect(ret).equal(
+`.red._prefix_bold
+{
+	color: red;
+}`
+      )
+    })
+
+    it('local class name with ! escape 2', function() {
+      var ret = cssobj({'.!red .!green .bold':{
+        'color':'red',
+      }}, {prefix: '_prefix_'}).css.trim()
+      expect(ret).equal(
+`.red .green ._prefix_bold
+{
+	color: red;
 }`
       )
     })
